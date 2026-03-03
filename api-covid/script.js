60f4e4a9695906c07b41cb9e38bb9db31961ace7
Adryan Senac <adryanisaque@gmail.com>
2026-03-03 20:54:00 -0300

    feat: adiciona select e estados de escolha e js pegando o valor

--- api-covid/script.js
+++ api-covid/script.js
@@ -1,1 +1,7 @@
+const select= document.querySelector('#uf')
 const btnBuscar = document.querySelector('#btnBuscar')
+
+btnBuscar.addEventListener('click', ()=>{
+    const valorSelecionado= select.value
+    console.log(valorSelecionado) 
+})
