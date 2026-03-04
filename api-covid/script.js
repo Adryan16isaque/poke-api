88284718b14db5baff1d556089ce4647155adf83
Adryan Senac <adryanisaque@gmail.com>
2026-03-03 21:04:34 -0300

    feat: verifica o status da api e exibe

--- api-covid/script.js
+++ api-covid/script.js
@@ -1,7 +1,22 @@
-const select= document.querySelector('#uf')
+const select = document.querySelector('#uf')
 const btnBuscar = document.querySelector('#btnBuscar')
+const resultadoStatus = document.querySelector('#status')
 
-btnBuscar.addEventListener('click', ()=>{
-    const valorSelecionado= select.value
-    console.log(valorSelecionado) 
+verificarStatus()
+function verificarStatus() {
+    fetch("https://covid19-brazil-api.now.sh/api/status/v1", {
+    })
+        .then(response => response.json())
+        .then(dados => {
+            let status = dados.status
+            status == 'ok' ? resultadoStatus.innerHTML = `ONLINE` : resultadoStatus.innerHTML = `OFFLINE`
+        })
+        .catch(err => console.error(err));
+
+
+}
+
+btnBuscar.addEventListener('click', () => {
+    const valorSelecionado = select.value
+    console.log(valorSelecionado)
 })
